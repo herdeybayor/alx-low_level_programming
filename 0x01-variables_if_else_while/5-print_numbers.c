56e34ebaaa038979d0ff93778091b5c177dac507
herdeybayor <herdeybayor4real@gmail.com>
2023-03-18 00:35:14 +0000

    task-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - docs goes here
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
