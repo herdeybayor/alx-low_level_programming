9e137e1adb7dcdeda43b368fa0f790f824f41f3c
herdeybayor <herdeybayor4real@gmail.com>
2023-03-18 00:40:37 +0000

    task-7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - docs goes here
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l <= 'a'; l--)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
