2cb4a39b8ab57bfbafe7f1fd1399fdd45c448cf7
herdeybayor <herdeybayor4real@gmail.com>
2023-03-18 00:26:27 +0000

    task-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - docs goes here
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	for (l = 'A'; l <= 'Z'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
