32d398a629706091a41365b994d55971acb0c39e
herdeybayor <herdeybayor4real@gmail.com>
2023-03-18 00:49:21 +0000

    task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
