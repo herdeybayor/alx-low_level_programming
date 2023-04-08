916f667ca0f89be06c80cf3ed2a872bc2e4d9062
herdeybayor <herdeybayor4real@gmail.com>
2023-04-08 02:54:18 +0000

    fix betty errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * print_name - prints the current program name
+ * main - prints the current program name
  * @argc: argument counts
  * @argv: argument vector
  *
  * Return: Always 0 (Success)
  */
 
-int main(int argc __attribute((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
