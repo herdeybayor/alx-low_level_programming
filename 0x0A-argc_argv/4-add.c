f8589dcd2081a56bd081ed553e51c0717d320269
herdeybayor <herdeybayor4real@gmail.com>
2023-04-09 00:12:37 +0000

    task-4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * argc - number of arguments
+ * argv - array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char **argv)
+{
+	int i, n, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		n = atoi(argv[i]);
+
+		if (!n)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += n;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
