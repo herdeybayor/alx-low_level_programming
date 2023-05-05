60d07470e262dec6db2503e320e6cbec06be9ad7
herdeybayor <shereefdeen4real@gmail.com>
2023-05-05 05:27:26 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,5 +10,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    printf("Hello World\n");
+    unsigned int result = 0;
+	unsigned int bit = 1;
+
+	if (b == NULL)
+		return (0);
+
+	if (int i = 0; b[i] != '\0'; i++) {
+		if (b[i] == '1')
+			result += bit;
+		else if (b[i] != '0')
+			return (0);
+		bit <<= 1;
+	}
+
+	return (result);
 }
