cded633419341c0b63832c2c586676802ab6517e
herdeybayor <shereefdeen4real@gmail.com>
2023-05-09 03:21:00 +0100

    task-0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t read_bytes, write_bytes;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	read_bytes = read(fd, buffer, letters);
+	close(fd);
+	if (read_bytes == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	free(buffer);
+	if (write_bytes != read_bytes)
+		return (0);
+
+	return (write_bytes);
+}
